bbe613ec7c743361df5ab8396aa92ca95dce38c0
shibal1234 <malikkiyaumiddin123@gmail.com>
2026-08-11 09:58:51 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personality Web</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body class="container-md">
+
+    <header style="text-align: center;" class="border border-dark-subtle-5 mt-3 .bg-danger">
+        <h1>---[Personal Web]---</h1>
+    </header>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-md" style="background-color: #b750e7; background-image: linear-gradient(to right, #8e7ad6, #4e31cf); border: 1px solid #000; border-radius: 5px;">
+            <a class="navbar-brand" href="index.php">Home</a>
+            <a class="navbar-brand" href="biodata.php">About</a>
+            <a class="navbar-brand" href="portofolio.php">Portofolio</a>
+        </div>
+    </nav>
+
+    <section style="display: flex; gap: 20px; margin-top: 20px;">
+        <div class="card" style="width: 18rem;">
+            <img src="princess.jpg" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae tempore in repudiandae blanditiis quo corporis sint esse, voluptates odio sapiente et quidem maxime aliquam eius iste, laboriosam veniam deserunt nobis dolore temporibus autem, perferendis ipsam similique aspernatur? Repellendus saepe consectetur recusandae consequatur. Cumque labore delectus quidem debitis excepturi! Deserunt dignissimos laboriosam molestias. Fugiat ullam magni ducimus nihil, perspiciatis nobis quam sit vel delectus consectetur hic reprehenderit iusto odio cumque, ea nostrum inventore repudiandae adipisci distinctio quidem quisquam necessitatibus eligendi numquam dolor! Placeat facilis eius nemo, impedit in repellat sapiente vel obcaecati dolorum maxime laudantium cum modi quasi unde dicta hic.</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="princess.jpg" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae tempore in repudiandae blanditiis quo corporis sint esse, voluptates odio sapiente et quidem maxime aliquam eius iste, laboriosam veniam deserunt nobis dolore temporibus autem, perferendis ipsam similique aspernatur? Repellendus saepe consectetur recusandae consequatur. Cumque labore delectus quidem debitis excepturi! Deserunt dignissimos laboriosam molestias. Fugiat ullam magni ducimus nihil, perspiciatis nobis quam sit vel delectus consectetur hic reprehenderit iusto odio cumque, ea nostrum inventore repudiandae adipisci distinctio quidem quisquam necessitatibus eligendi numquam dolor! Placeat facilis eius nemo, impedit in repellat sapiente vel obcaecati dolorum maxime laudantium cum modi quasi unde dicta hic.</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem; ">
+            <ul>
+                <li>Judul Artikel 1</li>
+                <br>
+                <li>Judul Artikel 2</li>
+                <br>
+                <li>Judul Artikel 3</li>
+                <br>
+                <li>Judul Artikel 4</li>
+                <br>
+                <li>Judul Artikel 5</li>
+            </ul>
+        </div>
+    </section>
+<br>
+    <footer style="text-align: center;" class="border border-dark-subtle mb-3">
+        <p>&copy; 2026 personal web, ALL RIGHT deserve</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ portofolio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personality Web</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body class="container-md">
+
+    <header style="text-align: center;" class="border border-dark-subtle-5 mt-3">
+        <h1>---[Personal Web]---</h1>
+    </header>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-md" style="background-color: #b750e7; background-image: linear-gradient(to right, #8e7ad6, #4e31cf); border: 1px solid #000; border-radius: 5px;">
+            <a class="navbar-brand" href="index.php">Home</a>
+            <a class="navbar-brand" href="biodata.php">About</a>
+            <a class="navbar-brand" href="portofolio.php">Portofolio</a>
+        </div>
+    </nav>
+    <footer style="text-align: center;" class="border border-dark-subtle mb-3">
+        <p>&copy; 2026 personal web, ALL RIGHT deserve</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
